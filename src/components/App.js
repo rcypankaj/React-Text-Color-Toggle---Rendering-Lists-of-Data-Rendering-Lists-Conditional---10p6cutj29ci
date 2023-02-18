f875e9d721d1e12708db6d0ba09aaf7697f45d7e
vivek kumar prasad <vivekprasad232@gmail.com>
2023-02-18 09:58:22 +0530

    commit 2

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,14 @@ import React, {useEffect, useState} from 'react'
 import '../styles/App.css';
 
 const App = () => {
-//code here 
+//code here
+  function tFunc(e){
+    const elem=e.target.parentNode.querySelector('.redColor').classList.toggle('blueColor');
+  }
   return (
     <div id="main">
-      <p className={} >Newton School</p>
-      <button id='button' onClick={}>Change Style</button>
+      <p className={'redColor'} >Newton School</p>
+      <button id='button' onClick={tFunc}>Change Style</button>
     </div>
   )
 }
